592f0f898d7439db445926ef547783e0f23ba276
Viji <aathavan.bagawan@gmail.com>
2018-02-25 18:37:19 -0500

    class styling working on change

--- dev/scripts/currentBattle.js
+++ dev/scripts/currentBattle.js
@@ -4,7 +4,7 @@ class Currentbattle extends React.Component {
     render () {
         console.log(this.props)
         return (
-            <div>
+            <div className={this.props.battle.Title.replace(' ','-').toLowerCase()}>
                 <p>{this.props.battle.Title}</p>
                 <p>{this.props.battle.Prelude}</p>
                 <p>{this.props.battle.Events}</p>
